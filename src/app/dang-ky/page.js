cd3b9efb7b86984de044257dab57d42c074667a4
thanhDo220404 <chodenmot@gmail.com>
2024-10-04 22:09:06 +0700

    lamnaylamkia

--- src/app/dang-ky/page.js
+++ src/app/dang-ky/page.js
@@ -1,22 +1,42 @@
 "use client";
+
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import useSWR from "swr";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 const Alert = ({ message, countdown, onClose }) => (
-  <div className="alert alert-light border border-success mt-3" role="alert">
-    <h1 className="alert-heading text-success">
-      <i className="bi bi-check-circle-fill"></i>
-    </h1>
+  <div
+    className="alert alert-light border border-success position-absolute"
+    role="alert"
+    style={{
+      top: "20%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      zIndex: 1000,
+    }}
+  >
+    <div className="m-auto">
+      <h1 className="alert-heading text-success">
+        <i className="bi bi-check-circle-fill"></i>
+      </h1>
+    </div>
     <p>{message}</p>
     <hr />
     <p className="my-3 fs-3 text-danger">
       Hãy kiểm tra email và xác nhận tài khoản
     </p>
     <p className="mb-0">Thời gian còn lại: {countdown} giây</p>
-    {countdown <= 0 && onClose()}
+    <div className="position-absolute w-100 h-100 top-0 start-0">
+      <div
+        className="fs-4 ms-2 text-danger position-absolute top-0 start-100"
+        onClick={onClose}
+      >
+        <i class="bi bi-x-circle-fill"></i>
+      </div>
+    </div>
   </div>
 );
 
@@ -26,11 +46,9 @@ export default function Register() {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const { data, isLoading, error } = useSWR(
-    "http://localhost:2204/users",
-    fetcher,
-    { refreshInterval: 6000 }
-  );
+  const { data, isLoading, error } = useSWR(`${apiUrl}/users`, fetcher, {
+    refreshInterval: 6000,
+  });
 
   const [isLoadingSubmit, setIsLoadingSubmit] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -73,7 +91,7 @@ export default function Register() {
   const onSubmit = async (formData) => {
     setIsLoadingSubmit(true);
     try {
-      const res = await fetch("http://localhost:2204/users/register", {
+      const res = await fetch(`${apiUrl}/users/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
@@ -91,11 +109,11 @@ export default function Register() {
   };
 
   return (
-    <div className="row m-auto w-100 text-center">
+    <div className="row m-auto w-100 text-center position-relative">
       <div className="col border border-dark p-5">
         <h1>Đăng ký</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
-          {/* Các trường nhập liệu */}
+          {/* Tên */}
           <div className="form-floating mb-3">
             <input
               type="text"
@@ -110,6 +128,7 @@ export default function Register() {
             )}
           </div>
 
+          {/* Email */}
           <div className="form-floating mb-3">
             <input
               type="email"
@@ -131,6 +150,7 @@ export default function Register() {
             )}
           </div>
 
+          {/* Số điện thoại */}
           <div className="form-floating mb-3">
             <input
               type="text"
@@ -147,12 +167,13 @@ export default function Register() {
             )}
           </div>
 
+          {/* Mật khẩu */}
           <div className="form-floating mb-3">
             <input
               type="password"
               className={`form-control ${errors.pass ? "is-invalid" : ""}`}
               id="floatingPassword"
-              placeholder="Password"
+              placeholder="Mật khẩu"
               {...register("pass", {
                 required: "Mật khẩu không được bỏ trống",
                 minLength: {
@@ -167,6 +188,7 @@ export default function Register() {
             )}
           </div>
 
+          {/* Nút đăng ký */}
           <button
             type="submit"
             className="w-100 py-3 rounded mb-3"
@@ -175,6 +197,7 @@ export default function Register() {
             Tạo tài khoản
           </button>
 
+          {/* Hiển thị trạng thái đang tải */}
           {isLoadingSubmit && (
             <div className="text-center text-success py-2">
               <div className="spinner-border"></div>
@@ -183,6 +206,7 @@ export default function Register() {
           )}
         </form>
 
+        {/* Hiển thị thông báo thành công */}
         {showAlert && (
           <Alert
             message="Bạn đã đăng ký thành công tài khoản."
@@ -191,6 +215,7 @@ export default function Register() {
           />
         )}
 
+        {/* Liên kết tới trang đăng nhập */}
         <div className="row mb-3">
           <div className="col">
             <hr />
@@ -204,8 +229,8 @@ export default function Register() {
           Tôi đã có tài khoản
         </a>
       </div>
-      <div className="col border border-dark d-none d-md-flex ">
-        <img src="/images/logo4x.png" alt="" className="w-50 m-auto" />
+      <div className="col border border-dark d-none d-md-flex">
+        <img src="/images/logo4x.png" alt="Logo" className="w-50 m-auto" />
       </div>
     </div>
   );
