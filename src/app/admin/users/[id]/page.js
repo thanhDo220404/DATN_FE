7beb7b62103610c9f24211fd7767e69fb5d1b7e9
hongnhung0502 <nhungnguyen.050204@gmail.com>
2024-11-11 18:51:51 +0700

    chỉnh sửa trang user-ad

--- src/app/admin/users/[id]/page.js
+++ src/app/admin/users/[id]/page.js
@@ -16,15 +16,15 @@ export default function ({ params }) {
     setUser(result.User);
   };
 
-  const fetchUserAddress = async (id) => {
-    const result = await getAllByUserId(id);
-    setUserAddress(result);
-  };
-
   const fetchUserOrders = async (id) => {
     const result = await getOrdersByUserId(id);
     console.log(result);
     setUserOrders(result);
+  };
+
+  const fetchUserAddress = async (id) => {
+    const result = await getAllByUserId(id);
+    setUserAddress(result);
   };
 
   useEffect(() => {
@@ -42,159 +42,160 @@ export default function ({ params }) {
     : "Chưa có địa chỉ mặt định";
 
   return (
+   
     <>
-      <div className="app-title">
-        <ul className="app-breadcrumb breadcrumb side">
-          <li className="breadcrumb-item active">
-            <a href="#">
-              <b>Chi tiết người dùng</b>
-            </a>
-          </li>
-        </ul>
-        <div id="clock" />
-      </div>
-      <div className="row">
-        <div className="col-4">
-          <div className="row">
-            <div className="col-12">
-              <div className="bg-white shadow p-3">
-                <h4>Hồ sơ</h4>
-                <div className="text-center">
-                  <img
-                    src={`http://localhost:2204/img/user/${user.image}`}
-                    alt=""
-                    className="w-25 rounded"
-                  />
-                  <div className="mt-3">{user._id}</div>
-                  <div className="mt-3">{user.name}</div>
+    <div className="app-title">
+      <ul className="app-breadcrumb breadcrumb side">
+        <li className="breadcrumb-item active">
+          <a href="#">
+            <b>Chi tiết người dùng</b>
+          </a>
+        </li>
+      </ul>
+      <div id="clock" />
+    </div>
+    <div className="row">
+      <div className="col-4">
+        <div className="row">
+          <div className="col-12">
+            <div className="bg-white shadow p-3">
+              <h4>Hồ sơ</h4>
+              <div className="text-center">
+                <img
+                  src={`http://localhost:2204/img/user/${user.image}`}
+                  alt=""
+                  className="w-25 rounded"
+                />
+                <div className="mt-3">{user._id}</div>
+                <div className="mt-3">{user.name}</div>
+              </div>
+              <div className="text-start mt-3">
+                <div className="d-flex mb-3">
+                  <i className="bi bi-phone me-3"></i>
+                  {user.phone}
                 </div>
-                <div className="text-start mt-3">
-                  <div className="d-flex mb-3">
-                    <i className="bi bi-phone me-3"></i>
-                    {user.phone}
-                  </div>
-                  <div className="d-flex mb-3">
-                    <i className="bi bi-envelope me-3"></i>
-                    <span>{user.email}</span>
-                  </div>
-                  <div className="d-flex mb-3">
-                    <i className="bi bi-geo-alt me-3"></i>
-                    <span>{addressToDisplay}</span>
-                  </div>
+                <div className="d-flex mb-3">
+                  <i className="bi bi-envelope me-3"></i>
+                  <span>{user.email}</span>
+                </div>
+                <div className="d-flex mb-3">
+                  <i className="bi bi-geo-alt me-3"></i>
+                  <span>{addressToDisplay}</span>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="col-8">
-          <div className="row bg-white shadow py-3">
-            <h4>Địa chỉ của người dùng</h4>
-            <div className="col-12">
-              <table className="table table-striped">
-                <thead>
-                  <tr>
-                    <th>Tên</th>
-                    <th>SĐT</th>
-                    <th>Địa chỉ đầy đủ</th>
+      </div>
+      <div className="col-8">
+        <div className="row bg-white shadow py-3">
+          <h4>Địa chỉ của người dùng</h4>
+          <div className="col-12">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>Tên</th>
+                  <th>SĐT</th>
+                  <th>Địa chỉ đầy đủ</th>
+                </tr>
+              </thead>
+              <tbody>
+                {userAddress.map((address, index) => (
+                  <tr key={index}>
+                    <td>{address.name}</td>
+                    <td>{address.phone}</td>
+                    <td>
+                      {address.specific_address},{" "}
+                      {address.address.district.ward.prefix}{" "}
+                      {address.address.district.ward.name},{" "}
+                      {address.address.district.name}, {address.address.name}
+                    </td>
                   </tr>
-                </thead>
-                <tbody>
-                  {userAddress.map((address, index) => (
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="row bg-white shadow py-3 mt-3">
+          <h4>Đơn hàng của người dùng</h4>
+          <div className="col-12">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>Tên sản phẩm</th>
+                  <th>Số lượng</th>
+                  <th>Tổng tiền</th>
+                  <th>Trạng thái</th>
+                  <th>Tính năng</th>
+                </tr>
+              </thead>
+              <tbody>
+                {userOrders.map((order, index) => {
+                  // Lấy tên sản phẩm đầu tiên
+                  const productName = order.products[0].name;
+
+                  // Tính tổng số lượng sản phẩm
+                  const totalQuantity = order.products.reduce(
+                    (total, product) => total + product.quantity,
+                    0
+                  );
+
+                  // Tính tổng số sản phẩm còn lại (sản phẩm khác ngoài sản phẩm đầu tiên)
+                  const otherProductsCount = order.products.length - 1;
+
+                  // Nếu có các sản phẩm khác thì hiển thị thêm
+                  const displayText =
+                    otherProductsCount > 0
+                      ? `${productName} - ${order.products[0].quantity} cái và ${otherProductsCount} sản phẩm khác...`
+                      : `${productName} - ${order.products[0].quantity} cái`;
+
+                  const totalAmount = order.order_total;
+                  const status = order.order_status;
+
+                  return (
                     <tr key={index}>
-                      <td>{address.name}</td>
-                      <td>{address.phone}</td>
+                      <td>{displayText}</td>
+                      <td>{totalQuantity}</td>
+                      <td>{totalAmount.toLocaleString()} VNĐ</td>
                       <td>
-                        {address.specific_address},{" "}
-                        {address.address.district.ward.prefix}{" "}
-                        {address.address.district.ward.name},{" "}
-                        {address.address.district.name}, {address.address.name}
+                        <span
+                          className={`badge ${
+                            status.name === "Đã giao hàng"
+                              ? "bg-success"
+                              : status.name === "Chờ xử lý"
+                              ? "bg-info"
+                              : status.name === "Đang giao hàng"
+                              ? "bg-warning"
+                              : status.name === "Đã hủy"
+                              ? "bg-danger"
+                              : "bg-primary"
+                          }`}
+                        >
+                          {status.name}
+                        </span>
+                      </td>
+                      <td>
+                        <Link href={`/admin/don-hang/sua/${order._id}`}>
+                          <button
+                            className="btn btn-primary btn-sm edit"
+                            type="button"
+                            title="Sửa"
+                            id="show-emp"
+                            style={{ width: "20px", margin: "5px" }}
+                          >
+                            <i className="bi bi-pencil" />
+                          </button>
+                        </Link>
                       </td>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <div className="row bg-white shadow py-3 mt-3">
-            <h4>Đơn hàng của người dùng</h4>
-            <div className="col-12">
-              <table className="table table-striped">
-                <thead>
-                  <tr>
-                    <th>Tên sản phẩm</th>
-                    <th>Số lượng</th>
-                    <th>Tổng tiền</th>
-                    <th>Trạng thái</th>
-                    <th>Tính năng</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {userOrders.map((order, index) => {
-                    // Lấy tên sản phẩm đầu tiên
-                    const productName = order.products[0].name;
-
-                    // Tính tổng số lượng sản phẩm
-                    const totalQuantity = order.products.reduce(
-                      (total, product) => total + product.quantity,
-                      0
-                    );
-
-                    // Tính tổng số sản phẩm còn lại (sản phẩm khác ngoài sản phẩm đầu tiên)
-                    const otherProductsCount = order.products.length - 1;
-
-                    // Nếu có các sản phẩm khác thì hiển thị thêm
-                    const displayText =
-                      otherProductsCount > 0
-                        ? `${productName} - ${order.products[0].quantity} cái và ${otherProductsCount} sản phẩm khác...`
-                        : `${productName} - ${order.products[0].quantity} cái`;
-
-                    const totalAmount = order.order_total;
-                    const status = order.order_status;
-
-                    return (
-                      <tr key={index}>
-                        <td>{displayText}</td>
-                        <td>{totalQuantity}</td>
-                        <td>{totalAmount.toLocaleString()} VNĐ</td>
-                        <td>
-                          <span
-                            className={`badge ${
-                              status.name === "Đã giao hàng"
-                                ? "bg-success"
-                                : status.name === "Chờ xử lý"
-                                ? "bg-info"
-                                : status.name === "Đang giao hàng"
-                                ? "bg-warning"
-                                : status.name === "Đã hủy"
-                                ? "bg-danger"
-                                : "bg-primary"
-                            }`}
-                          >
-                            {status.name}
-                          </span>
-                        </td>
-                        <td>
-                          <Link href={`/admin/don-hang/sua/${order._id}`}>
-                            <button
-                              className="btn btn-primary btn-sm edit"
-                              type="button"
-                              title="Sửa"
-                              id="show-emp"
-                              style={{ width: "20px", margin: "5px" }}
-                            >
-                              <i className="bi bi-pencil" />
-                            </button>
-                          </Link>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-    </>
+    </div>
+  </>
   );
 }
--- src/app/admin/users/page.js
+++ src/app/admin/users/page.js
@@ -18,94 +18,95 @@ export default function Users() {
   }, []);
 
   return (
+   
     <>
-      <div className="app-title">
-        <ul className="app-breadcrumb breadcrumb side">
-          <li className="breadcrumb-item active">
-            <a href="#">
-              <b>Danh sách khách hàng</b>
-            </a>
-          </li>
-        </ul>
-        <div id="clock" />
-      </div>
-      <div className="row">
-        <div className="col-md-12">
-          <div className="tile">
-            <div className="tile-body">
-              <div className="row element-button">
-                <div className="col-sm-2">
-                  {/* <a
-                    className="btn btn-delete btn-sm nhap-tu-file"
-                    type="button"
-                    title="Nhập"
-                  >
-                    <i className="bi bi-file-earmark-arrow-up" /> Tải từ file
-                  </a> */}
-                </div>
-                {/* Các button khác */}
+    <div className="app-title">
+      <ul className="app-breadcrumb breadcrumb side">
+        <li className="breadcrumb-item active">
+          <a href="#">
+            <b>Danh sách khách hàng</b>
+          </a>
+        </li>
+      </ul>
+      <div id="clock" />
+    </div>
+    <div className="row">
+      <div className="col-md-12">
+        <div className="tile">
+          <div className="tile-body">
+            <div className="row element-button">
+              <div className="col-sm-2">
+                {/* <a
+                  className="btn btn-delete btn-sm nhap-tu-file"
+                  type="button"
+                  title="Nhập"
+                >
+                  <i className="bi bi-file-earmark-arrow-up" /> Tải từ file
+                </a> */}
               </div>
-              <table
-                className="table table-hover table-bordered js-copytextarea table-align-center"
-                cellPadding={0}
-                cellSpacing={0}
-                border={0}
-                id="sampleTable"
-              >
-                <thead>
-                  <tr>
-                    <th>ID khách hàng</th>
-                    <th width={150}>Họ và tên</th>
-                    <th width={20}>Ảnh</th>
-                    <th>Email</th>
-                    <th>SĐT</th>
-                    <th>Tính năng</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {listUsers.length > 0 ? (
-                    listUsers.map((user) => (
-                      <tr key={user._id}>
-                        <td>{user._id}</td>
-                        <td>{user.name}</td>
-                        <td>
-                          <img
-                            className="img-card-person"
-                            src={`http://localhost:2204/img/user/${user.image}`}
-                            alt="User Avatar"
-                            width="50"
-                          />
-                        </td>
-                        <td>{user.email}</td>
-                        <td>{user.phone}</td>
-                        <td>
-                          <Link href={`/admin/users/${user._id}`}>
-                            <button
-                              className="btn btn-primary btn-sm edit"
-                              type="button"
-                              title="Sửa"
-                              id="show-emp"
-                              style={{ width: "20px", margin: "5px" }}
-                            >
-                              <i className="bi bi-pencil" />
-                            </button>
-                          </Link>
-                        </td>
-                      </tr>
-                    ))
-                  ) : (
-                    <tr>
-                      <td colSpan="6" style={{ textAlign: "center" }}>
-                        Không có dữ liệu người dùng
+              {/* Các button khác */}
+            </div>
+            <table
+              className="table table-hover table-bordered js-copytextarea table-align-center"
+              cellPadding={0}
+              cellSpacing={0}
+              border={0}
+              id="sampleTable"
+            >
+              <thead>
+                <tr>
+                  <th>ID khách hàng</th>
+                  <th width={150}>Họ và tên</th>
+                  <th width={20}>Ảnh</th>
+                  <th>Email</th>
+                  <th>SĐT</th>
+                  <th>Tính năng</th>
+                </tr>
+              </thead>
+              <tbody>
+                {listUsers.length > 0 ? (
+                  listUsers.map((user) => (
+                    <tr key={user._id}>
+                      <td>{user._id}</td>
+                      <td>{user.name}</td>
+                      <td>
+                        <img
+                          className="img-card-person"
+                          src={`http://localhost:2204/img/user/${user.image}`}
+                          alt="User Avatar"
+                          width="50"
+                        />
+                      </td>
+                      <td>{user.email}</td>
+                      <td>{user.phone}</td>
+                      <td>
+                        <Link href={`/admin/users/${user._id}`}>
+                          <button
+                            className="btn btn-primary btn-sm edit"
+                            type="button"
+                            title="Sửa"
+                            id="show-emp"
+                            style={{ width: "20px", margin: "5px" }}
+                          >
+                            <i className="bi bi-pencil" />
+                          </button>
+                        </Link>
                       </td>
                     </tr>
-                  )}
-                </tbody>
-              </table>
-            </div>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan="6" style={{ textAlign: "center" }}>
+                      Không có dữ liệu người dùng
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-    </>
+    </div>
+  </>
   );
 }
