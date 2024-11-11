1db8bbcbef1d261de00fdd62bee71f9e99167e3a
hongnhung0502 <nhungnguyen.050204@gmail.com>
2024-11-11 18:57:38 +0700

    trang header

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -35,6 +35,14 @@ export default function Header() {
     setIsLoggedIn(false);
     window.location.reload();
   };
+  // Xử lý sự kiện tìm kiếm
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    if (searchKeyword.trim()) {
+      router.push(`/tim-kiem?keywords=${encodeURIComponent(searchKeyword)}`);
+      setSearchKeyword(""); // Xóa chữ trong ô input sau khi tìm kiếm
+    }
+  };
 
   const fetchCategories = async () => {
     const result = await getAllCategories();
@@ -58,63 +66,121 @@ export default function Header() {
     }
   }, []);
 
-  // Xử lý sự kiện tìm kiếm
-  const handleSearchSubmit = (e) => {
-    e.preventDefault();
-    if (searchKeyword.trim()) {
-      router.push(`/tim-kiem?keywords=${encodeURIComponent(searchKeyword)}`);
-      setSearchKeyword(""); // Xóa chữ trong ô input sau khi tìm kiếm
-    }
-  };
+  
 
   return (
+   
     <header className="menu-header">
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-dark">
-          {/* Icons cho mobile */}
-          <div className="nav-icons d-flex d-lg-none align-items-center">
-            <Link href="/dang-nhap">
-              <FaUser className="nav-icon text-light fs-5 me-3" />
-            </Link>
-            <Link href="/gio-hang">
-              <FaShoppingCart className="nav-icon text-light fs-5" />
-            </Link>
-          </div>
-
-          {/* Logo */}
-          <Link className="navbar-brand" href="/">
-            <img
-              src="/images/logo.png"
-              alt="Logo"
-              style={{ width: "150px", height: "70px" }}
-            />
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-dark">
+        {/* Icons cho mobile */}
+        <div className="nav-icons d-flex d-lg-none align-items-center">
+          <Link href="/dang-nhap">
+            <FaUser className="nav-icon text-light fs-5 me-3" />
           </Link>
+          <Link href="/gio-hang">
+            <FaShoppingCart className="nav-icon text-light fs-5" />
+          </Link>
+        </div>
 
-          {/* Toggler cho mobile */}
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+        {/* Logo */}
+        <Link className="navbar-brand" href="/">
+          <img
+            src="/images/logo.png"
+            alt="Logo"
+            style={{ width: "150px", height: "70px" }}
+          />
+        </Link>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link" href="/">
-                  Trang chủ
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" href="/san-pham">
-                  Sản phẩm
-                </Link>
-              </li>
+        {/* Toggler cho mobile */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" href="/">
+                Trang chủ
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/san-pham">
+                Sản phẩm
+              </Link>
+            </li>
+            <div className="dropdown">
+              <Link
+                className="nav-link"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Danh mục
+                <i className="bi bi-caret-down-fill"></i>
+              </Link>
+              <ul className="dropdown-menu">
+                {categories.map((category) => (
+                  <li key={category._id}>
+                    <Link
+                      className="dropdown-item"
+                      href={`/danh-muc/${category._id}`}
+                    >
+                      {category.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <li className="nav-item">
+              <a className="nav-link d-flex" href="#">
+                Flash Sale
+                <img
+                  src="/images/fireFlashSale.png"
+                  width="20px"
+                  alt="Flash Sale"
+                />
+              </a>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="#">
+                Về chúng tôi
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="#">
+                Hỗ trợ
+              </Link>
+            </li>
+          </ul>
+
+          {/* Form tìm kiếm */}
+          <form className="d-flex me-2" onSubmit={handleSearchSubmit}>
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Tìm kiếm sản phẩm..."
+              aria-label="Search"
+              value={searchKeyword}
+              onChange={(e) => setSearchKeyword(e.target.value)}
+            />
+            <button className="btn btn-outline-light" type="submit">
+              <FaSearch />
+            </button>
+          </form>
+
+          {/* Icons cho desktop */}
+          <div className="nav-icons d-none d-lg-flex align-items-center">
+            {isLoggedIn ? (
               <div className="dropdown">
                 <Link
                   className="nav-link"
@@ -123,128 +189,64 @@ export default function Header() {
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
-                  Danh mục
-                  <i className="bi bi-caret-down-fill"></i>
+                  {userLoginInfo ? (
+                    <img
+                      width="32"
+                      height="32"
+                      className="rounded-circle me-2"
+                      src={`${apiUrl}/img/user/${userLoginInfo.image}`}
+                      alt=""
+                    />
+                  ) : (
+                    <FaUser className="nav-icon text-light fs-5 me-3" />
+                  )}
                 </Link>
                 <ul className="dropdown-menu">
-                  {categories.map((category) => (
-                    <li key={category._id}>
-                      <Link
-                        className="dropdown-item"
-                        href={`/danh-muc/${category._id}`}
-                      >
-                        {category.name}
-                      </Link>
+                  {isAdmin && (
+                    <li>
+                      <a className="dropdown-item" href="/admin">
+                        Quản lý website
+                      </a>
                     </li>
-                  ))}
+                  )}
+                  <li>
+                    <Link
+                      className="dropdown-item"
+                      href="/user/tai-khoan/ho-so"
+                    >
+                      Tài khoản của tôi
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="/user/don-mua">
+                      Đơn mua
+                    </Link>
+                  </li>
+                  <li>
+                    <button className="dropdown-item" onClick={handleLogout}>
+                      Đăng xuất
+                    </button>
+                  </li>
                 </ul>
               </div>
-              <li className="nav-item">
-                <a className="nav-link d-flex" href="#">
-                  Flash Sale
-                  <img
-                    src="/images/fireFlashSale.png"
-                    width="20px"
-                    alt="Flash Sale"
-                  />
-                </a>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" href="#">
-                  Về chúng tôi
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" href="#">
-                  Hỗ trợ
-                </Link>
-              </li>
-            </ul>
-
-            {/* Form tìm kiếm */}
-            <form className="d-flex me-2" onSubmit={handleSearchSubmit}>
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Tìm kiếm sản phẩm..."
-                aria-label="Search"
-                value={searchKeyword}
-                onChange={(e) => setSearchKeyword(e.target.value)}
-              />
-              <button className="btn btn-outline-light" type="submit">
-                <FaSearch />
-              </button>
-            </form>
-
-            {/* Icons cho desktop */}
-            <div className="nav-icons d-none d-lg-flex align-items-center">
-              {isLoggedIn ? (
-                <div className="dropdown">
-                  <Link
-                    className="nav-link"
-                    href="#"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    {userLoginInfo ? (
-                      <img
-                        width="32"
-                        height="32"
-                        className="rounded-circle me-2"
-                        src={`${apiUrl}/img/user/${userLoginInfo.image}`}
-                        alt=""
-                      />
-                    ) : (
-                      <FaUser className="nav-icon text-light fs-5 me-3" />
-                    )}
-                  </Link>
-                  <ul className="dropdown-menu">
-                    {isAdmin && (
-                      <li>
-                        <a className="dropdown-item" href="/admin">
-                          Quản lý website
-                        </a>
-                      </li>
-                    )}
-                    <li>
-                      <Link
-                        className="dropdown-item"
-                        href="/user/tai-khoan/ho-so"
-                      >
-                        Tài khoản của tôi
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="dropdown-item" href="/user/don-mua">
-                        Đơn mua
-                      </Link>
-                    </li>
-                    <li>
-                      <button className="dropdown-item" onClick={handleLogout}>
-                        Đăng xuất
-                      </button>
-                    </li>
-                  </ul>
-                </div>
-              ) : (
-                <Link href={`/buyer/dang-nhap?next=${pathname}`}>
-                  <FaUser className="nav-icon text-light fs-5 me-3" />
-                </Link>
-              )}
-              <Link href="/gio-hang" className="position-relative">
-                <FaShoppingCart className="nav-icon text-light fs-5" />
-                <span
-                  style={{ fontSize: "12px" }}
-                  className="position-absolute top-0 start-100 translate-middle badge border border-light rounded-circle bg-danger p-1"
-                >
-                  {totalItems}
-                </span>
+            ) : (
+              <Link href={`/buyer/dang-nhap?next=${pathname}`}>
+                <FaUser className="nav-icon text-light fs-5 me-3" />
               </Link>
-            </div>
+            )}
+            <Link href="/gio-hang" className="position-relative">
+              <FaShoppingCart className="nav-icon text-light fs-5" />
+              <span
+                style={{ fontSize: "12px" }}
+                className="position-absolute top-0 start-100 translate-middle badge border border-light rounded-circle bg-danger p-1"
+              >
+                {totalItems}
+              </span>
+            </Link>
           </div>
-        </nav>
-      </div>
-    </header>
+        </div>
+      </nav>
+    </div>
+  </header>
   );
 }
